2f2ddc3ce0172a5966510a448ea1671e89d047e7
slime_staff <72954973+Mountainkaio@users.noreply.github.com>
2025-04-01 16:04:23 -0300

    Add files via upload

---
+++ site_matemática/JS/script.js
@@ -0,0 +1,15 @@
+let card3_btn = document.getElementById("btn_card3");
+let card3 = document.getElementById("text_card");
+let card3_old_text = card3.textContent;
+let wait = document.getElementById("wait")
+
+card3_btn.addEventListener("click", function(){
+	if (card3.textContent == card3_old_text){
+		wait;
+		card3.textContent = 'cu hahaha';
+		
+	}else{
+		card3.textContent = card3_old_text;
+	}
+
+});
