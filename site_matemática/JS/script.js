c8efc1e97112af82491a13cf8281d31b4401cb4a
Filkiro <gomeslipeff8888@gmail.com>
2025-04-08 19:00:30 -0300

    modified:   "site_matem\303\241tica/CSS/style.css" 	modified:   "site_matem\303\241tica/HTML/index.html" 	new file:   "site_matem\303\241tica/IMG/360_F_565546717_sWznHWWi4iSS0guDqPBguCfCfSsLBFMq.jpg" 	modified:   "site_matem\303\241tica/JS/script.js"

--- site_matemática/JS/script.js
+++ site_matemática/JS/script.js
@@ -6,7 +6,7 @@ let wait = document.getElementById("wait")
 card3_btn.addEventListener("click", function(){
 	if (card3.textContent == card3_old_text){
 		wait;
-		card3.textContent = 'cu hahaha';
+		card3.textContent = 'hahaha';
 		
 	}else{
 		card3.textContent = card3_old_text;
